965c9a5a65f281b8f5b7238ff3b2320fbc88c4b4
Rajaraj1 <rr702678@gmail.com>
2022-04-24 02:17:03 +0530

    Profile Commit 3

---
+++ app/src/main/java/com/example/wish/Model/FriendModel.java
@@ -0,0 +1,17 @@
+package com.example.wish.Model;
+
+public class FriendModel {
+    int profile;
+
+    public FriendModel(int profile) {
+        this.profile = profile;
+    }
+
+    public int getProfile() {
+        return profile;
+    }
+
+    public void setProfile(int profile) {
+        this.profile = profile;
+    }
+}
